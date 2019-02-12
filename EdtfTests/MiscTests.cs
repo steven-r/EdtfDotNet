d0a0d1e9c2e39fef819d7c5090f65fbefeff0843
Stephen Reindl <4533301+steven-r@users.noreply.github.com>
2019-02-12 20:35:00 +0100

    Create test case for #12

---
+++ EdtfTests/MiscTests.cs
@@ -0,0 +1,18 @@
+﻿using Edtf;
+using NUnit.Framework;
+
+namespace EdtfTests
+{
+    [TestFixture]
+    public class MiscTests
+    {
+        [Test, Ignore("Test for #12")]
+        public void TestStatusAndPartStatusSync()
+        {
+            const string dateString = "X199";
+            var testDate = Edtf.DatePair.Parse(dateString);
+            Assert.IsTrue(testDate.StartValue.Year.Invalid);
+            Assert.AreEqual(DateStatus.Invalid, testDate.StartValue.Status);
+        }
+    }
+}
